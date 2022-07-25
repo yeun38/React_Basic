215997dbe7f89d26cebbef6dea0e28946c0ccc5f
yeun38 <choh1997@naver.com>
2022-07-25 16:25:36 +0900

    매개변수 list

--- bucket_list/src/BucketList.js
+++ bucket_list/src/BucketList.js
@@ -1,9 +1,34 @@
-import React from "react"; // 리액트 패키지에서 리액트 객체 가져온다
+// 리액트 패키지를 불러옵니다.
+import React from "react";
 
-const BucketList = (props) => {
-  console.log(props);
-  return <div>버킷 리스트</div>;
+// 함수형 컴포넌트는 이렇게 쓸 수도 있고
+// function Bucketlist(props){
+//     return (
+//         <div>버킷 리스트</div>
+//     );
+// }
+
+const BucketList = ({ list }) => {
+  // Quiz 1: my_list에 ['a', 'b', 'c'] 대신 부모 컴포넌트가 넘겨준 값을 넣으려면 어떻게 해야할까요?
+  const my_lists = list;
+
+  // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
+  return (
+    <div>
+      {
+        // js의 내장 함수 중 하나인 map입니다. 리스트의 갯수만큼 => 오른쪽 구문을 반복해요.
+        // 자세한 사용법은 아래 링크를 확인해주세요.
+        // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+        my_lists.map((list, index) => {
+          // 콘솔을 확인해봅시다 :)
+          console.log(list);
+          return <div key={index}>{list}</div>;
+        })
+      }
+    </div>
+  );
 };
 
-export { BucketList };
-export default BucketList; // 다른 파일에서 해당 컴포넌트를 불러서 사용할 수 있도록 함.
+// 우리가 만든 함수형 컴포넌트를 export 해줍니다.
+// export 해주면 다른 컴포넌트에서 BucketList 컴포넌트를 불러다 쓸 수 있어요.
+export default BucketList;
