c40f6d30a7ab45d90ad7254388ef41547d5a688c
yeun38 <choh1997@naver.com>
2022-07-25 16:26:10 +0900

    매개변수 props

--- bucket_list/src/BucketList.js
+++ bucket_list/src/BucketList.js
@@ -8,9 +8,9 @@ import React from "react";
 //     );
 // }
 
-const BucketList = ({ list }) => {
+const BucketList = (props) => {
   // Quiz 1: my_list에 ['a', 'b', 'c'] 대신 부모 컴포넌트가 넘겨준 값을 넣으려면 어떻게 해야할까요?
-  const my_lists = list;
+  const my_lists = props.list;
 
   // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
   return (
