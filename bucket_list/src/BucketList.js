584668947c04fb3f53204708b6947c2b7091c621
yeun38 <choh1997@naver.com>
2022-07-25 15:42:52 +0900

    함수형 component

---
+++ bucket_list/src/BucketList.js
@@ -0,0 +1,8 @@
+import React from "react"; // 리액트 패키지에서 리액트 객체 가져온다
+
+const BucketList = (props) => {
+  return <div>버킷 리스트</div>;
+};
+
+export { BucketList };
+export default BucketList; // 다른 파일에서 해당 컴포넌트를 불러서 사용할 수 있도록 함.
