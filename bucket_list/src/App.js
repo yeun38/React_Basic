fcdc2d4886c51a355d1261e720bd337456f11ac4
yeun38 <choh1997@naver.com>
2022-07-25 23:04:26 +0900

    styled-components

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import BucketList from "./BucketList";
-import "./style.css";
+// import "./style.css";
+import styled from "styled-components";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,15 +15,39 @@ class App extends React.Component {
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <h1>내 버킷리스트</h1>
+      <MyApp>
+        <MyContainer>
+          <Title>내 버킷리스트</Title>
           <hr className="line" />
           <BucketList list={this.state.list} />
-        </div>
-      </div>
+        </MyContainer>
+      </MyApp>
     );
   }
 }
+const MyApp = styled.div`
+  background-color: #eee;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+`;
+
+const MyContainer = styled.div`
+  background-color: #fff;
+  width: 50vw;
+  max-width: 350px;
+  margin: auto;
+  height: 80vh;
+
+  border: 1px solid #ddd;
+  border-radius: 3px;
+
+  padding: 16px;
+`;
+
+const Title = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
 
 export default App;
--- bucket_list/src/BucketList.js
+++ bucket_list/src/BucketList.js
@@ -1,5 +1,6 @@
 // 리액트 패키지를 불러옵니다.
 import React from "react";
+import styled from "styled-components";
 
 // 함수형 컴포넌트는 이렇게 쓸 수도 있고
 // function Bucketlist(props){
@@ -14,7 +15,7 @@ const BucketList = (props) => {
 
   // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
   return (
-    <div>
+    <ListStyle>
       {
         // js의 내장 함수 중 하나인 map입니다. 리스트의 갯수만큼 => 오른쪽 구문을 반복해요.
         // 자세한 사용법은 아래 링크를 확인해주세요.
@@ -22,17 +23,27 @@ const BucketList = (props) => {
         my_lists.map((list, index) => {
           // 콘솔을 확인해봅시다 :)
           console.log(list);
-          return (
-            <div className="list-item" key={index}>
-              {list}
-            </div>
-          );
+          return <Item key={index}>{list}</Item>;
         })
       }
-    </div>
+    </ListStyle>
   );
 };
 
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const Item = styled.div`
+padding: 16px;
+margin: 8px;
+background-color: aliceblue;
+font-weight: 700;
+}`;
 // 우리가 만든 함수형 컴포넌트를 export 해줍니다.
 // export 해주면 다른 컴포넌트에서 BucketList 컴포넌트를 불러다 쓸 수 있어요.
 export default BucketList;
