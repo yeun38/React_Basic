5857f129252ed42e4dc3c2289a36dc5d7ab2d3bb
yeun38 <choh1997@naver.com>
2022-07-26 11:06:16 +0900

    클래스형 state

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import BucketList from "./BucketList";
 // import "./style.css";
 import styled from "styled-components";
@@ -8,46 +7,57 @@ class App extends React.Component {
     super(props);
     // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
-      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+      count: 3,
     };
   }
 
+  componentDidMount() {}
+
+  addNemo = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  removeNemo = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    } else {
+      window.alert("네모가 없어요");
+    }
+  };
+
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
+    const nemo_count = Array.from({ length: this.state.count }, (v, i) => i);
+    console.log(nemo_count);
     return (
-      <MyApp>
-        <MyContainer>
-          <Title>내 버킷리스트</Title>
-          <hr className="line" />
-          <BucketList list={this.state.list} />
-        </MyContainer>
-      </MyApp>
+      <div className="App">
+        {nemo_count.map((n, i) => {
+          return (
+            <div
+              key={i} //key임의지정 오류를 적게 보기 위함.
+              style={{
+                width: "150px",
+                height: "150px",
+                backgroundColor: "#ddd",
+                margin: "10px",
+              }}
+            >
+              nemonemo mongmumi
+            </div>
+          );
+        })}
+        <div>
+          <button
+            onClick={() => {
+              this.addNemo();
+            }}
+          >
+            하나 추가
+          </button>
+          <button onClick={this.removeNemo}>하나 빼기</button>
+        </div>
+      </div>
     );
   }
 }
-const MyApp = styled.div`
-  background-color: #eee;
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-`;
-
-const MyContainer = styled.div`
-  background-color: #fff;
-  width: 50vw;
-  max-width: 350px;
-  margin: auto;
-  height: 80vh;
-
-  border: 1px solid #ddd;
-  border-radius: 3px;
-
-  padding: 16px;
-`;
-
-const Title = styled.h1`
-  color: slateblue;
-  text-align: center;
-`;
 
 export default App;
