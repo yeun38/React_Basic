520ca346c93a15dc8a50082f73a61f6194ff12bf
yeun38 <choh1997@naver.com>
2022-08-03 13:59:53 +0900

    settings

---
+++ todo-redux/src/redux/config/configStore.js
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+
+// const rootReducer = combineReducers({});
+// const store = createStore(rootReducer);
+
+const store = combineReducers({});
+export default createStore(store);
